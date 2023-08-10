d278f2475ff7edee691c59a0f17e0aa1a567c303
Nelmer De La Cruz <nelmerux@gmail.com>
2023-08-10 12:09:17 -0400

    Update hello.js

--- sketches/hello.js
+++ sketches/hello.js
@@ -1,13 +1,29 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [1080, 1080]
 };
 
 const sketch = () => {
+  let x, y, w, h;
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'blue';
+    context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    x = width * 0.5;
+    y = height * 0.5;
+    w = width * 0.6;
+    h = height * 0.1;
+
+    context.save();
+    context.translate(x, y);
+
+    context.strokeStyle = 'blue';
+    context.strokeRect(w * -0.5, h * -0.5, w, h);
+
+    context.restore();
+
   };
 };
 
